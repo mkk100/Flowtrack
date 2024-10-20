59f7084a7f3763ee0db2f25beb7b1173fd31e4d2
mkk100 <tommychimes2003@gmail.com>
2024-10-20 15:59:55 -0700

    setting up clerk

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@mantine/core";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="bg-black">
-      <Button variant="filled" color="violet">
-        Button
+      <Button>
+        <Link href="/sign-up">Sign In</Link>
       </Button>
     </div>
   );
