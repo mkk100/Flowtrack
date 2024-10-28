2657d2081a5a0770756cc075f9cf5ec73fc1d2ce
mkk100 <tommychimes2003@gmail.com>
2024-10-27 17:34:06 -0700

    registering sends the data to the db via backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const app = express();
 
@@ -7,16 +7,41 @@ app.use(express.json());
 
 // cors
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Content-Type", "Authorization");
   next();
 });
 
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany();
-  console.log(users);
-  console.log('GET /users');
-  res.json(users);
+app.post("/users", async (req, res) => {
+  console.log(req.body.id);
+  try {
+    const user = await prisma.user.create({
+      data: {
+        id: req.body.id.toString(),
+        username: req.body.username,
+        avatar: req.body.avatar,
+      },
+    });
+    res.status(200).json(user);
+  } catch {
+    res.status(400).json({ error: "can't save the data" });
+  }
+});
+
+app.put("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.update({
+      where: { id: id.toString() },
+      data: {
+        username: req.body.username,
+        avatar: req.body.avatar,
+      },
+    });
+    res.status(200).json(user);
+  } catch {
+    res.status(400).json({ error: "can't update the data" });
+  }
 });
 
 const PORT = process.env.PORT || 4000;
