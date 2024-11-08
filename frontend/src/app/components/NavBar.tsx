71082a51fe4737b0d89771b01ac9849479ae35dd
mkk100 <tommychimes2003@gmail.com>
2024-11-08 11:35:00 -0800

    Nav bar styling

--- frontend/src/app/components/NavBar.tsx
+++ frontend/src/app/components/NavBar.tsx
@@ -15,26 +15,21 @@ const links = [
 export function NavBar() {
   const [opened, { toggle }] = useDisclosure(false);
   const items = links.map((link) => (
-    <a
-      key={link.label}
-      href={link.link}
-      className={classes.link}
-      onClick={(event) => event.preventDefault()}
-    >
+    <Link key={link.label} href={link.link} className={classes.link}>
       {link.label}
-    </a>
+    </Link>
   ));
 
   return (
     // menu at the right that includes the user profile and sign out
-    <header className={classes.header + " item-center"}>
+    <header className={classes.header + " item-center" + " pb-2 pt-2"}>
       <div className={classes.inner}>
         <Group>
           <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-          <Button>
+          <div className={classes.logo}>
             <FontAwesomeIcon icon={faArrowsToEye} />
             <Link href="/">&nbsp;&nbsp;Flowtrack</Link>
-          </Button>
+          </div>
         </Group>
         <Group>
           <Group ml={50} gap={46} className={classes.links} visibleFrom="sm">
